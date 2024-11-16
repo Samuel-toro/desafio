b4637196c5b9ad4366df7fad39125e28fd5afad7
Samuel-toro <storohernandez8@gmail.com>
2024-11-16 14:56:22 -0400

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { DescripcionComponent } from './descripcion/descripcion.component';
-
-export const routes: Routes = [
-  { path: 'home', component: AppComponent },
-  { path: 'descripcion/:id', component: DescripcionComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
-];
